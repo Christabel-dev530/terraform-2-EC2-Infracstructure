c44ce435543b94f7ae0d683e2b344eebee8f0892
Christabel-dev530 <christabelnwachukwu06@gmail.com>
2026-08-14 09:14:36 +0100

    Updated

--- main.tf
+++ main.tf
@@ -32,7 +32,16 @@ resource "aws_subnet" "public_subnet2" {
     Name = "my-server-Public-Subnet2"
   }
 }
-# Route Table for Public Subnet
+# Public Subnet 2
+resource "aws_subnet" "public_subnet3" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr_block3
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "my-server-Public-Subnet3"
+  }
+}
+# Route Table for Public Subnet1
 resource "aws_route_table" "public_route_table1" {
   vpc_id = aws_vpc.main.id
   tags = {
@@ -44,11 +53,23 @@ resource "aws_route_table" "public_route_table1" {
     gateway_id = aws_internet_gateway.gw.id
   }
 }
-# Route Table for Public Subnet
+# Route Table for Public Subnet2
 resource "aws_route_table" "public_route_table2" {
   vpc_id = aws_vpc.main.id
   tags = {
     Name = "my-server-Public-RT2"
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+# Route Table for Public Subnet3
+resource "aws_route_table" "public_route_table3" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "my-server-Public-RT3"
   }
 
   route {
@@ -65,6 +86,11 @@ resource "aws_route_table_association" "public_assoc1" {
 resource "aws_route_table_association" "public_assoc2" {
   subnet_id      = aws_subnet.public_subnet2.id
   route_table_id = aws_route_table.public_route_table2.id
+}
+# Associate Public Route Table with Public Subnet3
+resource "aws_route_table_association" "public_assoc3" {
+  subnet_id      = aws_subnet.public_subnet3.id
+  route_table_id = aws_route_table.public_route_table3.id
 }
 # EC2 Instance in the Public Subnet 1
 resource "aws_instance" "web_public1" {
@@ -92,5 +118,19 @@ resource "aws_instance" "web_public2" {
   key_name                    = "chris"
   tags = {
     Name = "demo_project_server2"
+  }
+}
+# EC2 Instance in the Public Subnet 3
+resource "aws_instance" "web_public3" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  availability_zone           = var.avail_zone3
+  subnet_id                   = aws_subnet.public_subnet3.id
+  associate_public_ip_address = true
+  user_data                   = file("jenkins.sh")
+  vpc_security_group_ids      = [aws_security_group.allow_web.id]
+  key_name                    = "chris"
+  tags = {
+    Name = "demo_project_server3"
   }
 }
--- output.tf
+++ output.tf
@@ -4,3 +4,6 @@ output "ec2_public_ip1" {
 output "ec2_public_ip2" {
   value = aws_instance.web_public2.public_ip
 }
+output "ec2_public_ip3" {
+  value = aws_instance.web_public3.public_ip
+}
